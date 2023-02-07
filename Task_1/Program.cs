dba5ebaac9b9fcd5b8171cbb5e8901a1cf3cb9dd
DenniSergeich <damned_wolf@mail.ru>
2023-02-07 13:20:07 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int NumPow (int num1, int num2){
+    int numPow = num1;
+    for (int i = 1; i < num2; i++){
+        numPow *= num1;
+    }
+    return numPow;
+}
+Console.WriteLine($"Возведение числа {num1} в степень {num2} равно: {NumPow(num1,num2)}");
