46468bcb7d640caea5b55a47049965a55380b5ed
DenniSergeich <damned_wolf@mail.ru>
2023-02-08 14:49:59 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int SumNum(int num)
+{
+    if (num < 0)
+    {
+        num *= -1;
+    }
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма всех цифр в числе {num} равна = {SumNum(num)}");
