42b73089397bb9621dfa0209f84223b1f879a046
DenniSergeich <damned_wolf@mail.ru>
2023-02-08 19:50:17 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int num = 8;
+int[] array = GetBinArray(num);
+Console.Write($"[{String.Join(", ", array)}]");
+int[] GetBinArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
